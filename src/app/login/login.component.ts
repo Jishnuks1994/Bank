7546cac51b7e8387b702b380b21eb9b09974503e
unknown <jishnuks9421@gmail.com>
2023-07-13 10:46:16 +0530

    template binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
+import { HomepageComponent } from '../homepage/homepage.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css' ]
 })
 export class LoginComponent {
+  
   data="Happy Banking with us..."
   pdata="Enter Username"
+  acno:any=""
+  psw:any=""
 
-  login(){
-    alert("Login Clicked")
+  login(a:any,b:any){
+    console.log(a.value);
+    console.log(b.value);
     
+       
   }
 
 }
